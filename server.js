b8f171ef552f50b37dc5bbb559ddfac45e1961e4
helcio <h.andre@giap.pl>
2024-01-31 12:33:55 +0100

    Complete phases 01, 02

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const { Player } = require('./game/class/player');
-const { World } = require('./game/class/world');
+const { Player } = require("./game/class/player");
+const { World } = require("./game/class/world");
 
-const worldData = require('./game/data/basic-world-data');
-
+const worldData = require("./game/data/basic-world-data");
+const methods = {
+  POST: "POST",
+  GET: "GET",
+};
 let player;
 let world = new World();
 world.loadWorld(worldData);
-
+function availableRoomsOptions(arr) {
+  return arr.map((room) => `<option value="${room.id}">${room.name}</option>`);
+}
 const server = http.createServer((req, res) => {
-
   /* ============== ASSEMBLE THE REQUEST BODY AS A STRING =============== */
-  let reqBody = '';
-  req.on('data', (data) => {
+  let reqBody = "";
+  req.on("data", (data) => {
     reqBody += data;
   });
 
-  req.on('end', () => { // After the assembly of the request body is finished
+  req.on("end", () => {
+    // After the assembly of the request body is finished
     /* ==================== PARSE THE REQUEST BODY ====================== */
     if (reqBody) {
       req.body = reqBody
@@ -33,10 +38,41 @@ const server = http.createServer((req, res) => {
     }
 
     /* ======================== ROUTE HANDLERS ========================== */
+    const { method, url } = req;
+
     // Phase 1: GET /
+    if (method === methods.GET && url === "/") {
+      const htmlPage = fs.readFileSync("./views/new-player.html", "utf-8");
+      const resBody = htmlPage.replace(
+        /#{availableRooms}/g,
+        availableRoomsOptions(worldData.rooms)
+      );
+      res.statusCode = 200;
+      res.setHeader("Content-type", "text/html");
+      res.write(resBody);
+      return res.end();
+    }
 
     // Phase 2: POST /player
+    if (method === methods.POST && url === "/player") {
+      const { name, roomId } = req.body;
+      const playerStartingRoom = worldData.rooms.find(
+        (room) => room.id === Number(roomId)
+      );
+      if (name && playerStartingRoom) {
+        player = new Player(name, playerStartingRoom);
+      } else {
+        return res.end("Not a valid player name or room");
+      }
 
+      // response to add on phase 03
+      // const htmlPage = fs.readFileSync('./views/room.html')
+      // const resBody = htmlPage
+      res.statusCode = 201;
+      res.statusCode = 302;
+      res.setHeader("Location", `/rooms/${roomId}`);
+      return res.end();
+    }
     // Phase 3: GET /rooms/:roomId
 
     // Phase 4: GET /rooms/:roomId/:direction
@@ -44,9 +80,9 @@ const server = http.createServer((req, res) => {
     // Phase 5: POST /items/:itemId/:action
 
     // Phase 6: Redirect if no matching route handlers
-  })
+  });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
